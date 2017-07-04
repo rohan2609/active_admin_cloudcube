ea2d9bec5e99a2993177b4f1ca36b9d21bffaf11
Rohan <routrohan90@gmail.com>
2017-07-04 18:13:38 +0530

    added proc for custormr

--- app/admin/offline_payment.rb
+++ app/admin/offline_payment.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register OfflinePayment do
 	filter :plan, :label => 'Plan Name',  if: proc {Plan.all.map{|p| [p.plan_name,p.id]}}
-    filter :customer, :label => 'Customer Name', :collection => Customer.all.map{|c| [c.fullname,c.id]}
+    filter :customer, :label => 'Customer Name',  if: proc{Customer.all.map{|c| [c.fullname,c.id]}}
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -16,7 +16,7 @@ permit_params :customer_id,:plan_id,:amount,:date,:check_number,:bank_name,:date
 form do |f|
     f.inputs "Offline Details" do
       f.input :plan_id, :as => :select, if: proc {Plan.all.map{|p| [p.plan_name,p.id]}}
-      f.input :customer_id, :as => :select, :collection => Customer.all.map{|c| [c.fullname,c.id]}
+      f.input :customer_id, :as => :select, if: proc{Customer.all.map{|c| [c.fullname,c.id]}}
       f.input :bank_name
       f.input :check_number
       f.input :amount

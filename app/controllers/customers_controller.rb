33388c12a946555f568ca0c19257547bbfbae69d
Rohan <routrohan90@gmail.com>
2017-07-05 12:59:35 +0530

    added

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,28 +1,6 @@
 class CustomersController < InheritedResources::Base
 skip_before_action :verify_authenticity_token, :only => [:payu_return]
 
-def new
-  @customer = Customer.new
-end
-
-def update
-  @customer = Customer.find(params[:id])
-  @customer.update(customer_params)
-  redirect_to customers_path
-end
-
-  def create
-    @customer = Customer.new(customer_params)
-    if @customer.save
-    redirect_to customers_path
-  else
-    render 'new'
-  end
-  end
-
-  def show
-    @customer = Customer.find(params[:id])
-  end
 
   def confirm_to_pay
     @customer = Customer.find(params[:id])

67ed50b5464efc8d4443548c3820760beb1baee3
Rohan <routrohan90@gmail.com>
2017-07-05 18:22:53 +0530

    removed a line productio

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.assets.raise_runtime_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {

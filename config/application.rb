d9929d02f5a6361b3601ec3bb3e02937c9557498
Rohan <routrohan90@gmail.com>
2017-07-06 15:54:10 +0530

    added a config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module ActiveAdminCloudcube
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
+	config.assets.precompile += %w[active_admin.css active_admin.js print.css]
+	config.assets.precompile += %w[active_admin/print.css]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
